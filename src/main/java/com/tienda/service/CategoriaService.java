0244959e4dbe77d6550a8cab945f000d69553460
rnsanti <nsantiago20780@ufide.ac.cr>
2025-03-27 13:14:22 -0600

    Commit de la Clase 9
    Commit de la clase 9, lo subo hoy porque olvidé por completo hacerlo la semana pasada antes del caso

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -7,4 +7,13 @@ public interface CategoriaService {
     //Se obtiene el listado de la Base de Datos.
     public List<Categoria> getCategorias(boolean activos);
     
+    // Se obtiene un Categoria, a partir del id de un categoria
+    public Categoria getCategoria(Categoria categoria);
+    
+    // Se inserta un nuevo categoria si el id del categoria esta vacío
+    // Se actualiza un categoria si el id del categoria NO esta vacío
+    public void save(Categoria categoria);
+    
+    // Se elimina el categoria que tiene el id pasado por parámetro
+    public void delete(Categoria categoria);
 }
