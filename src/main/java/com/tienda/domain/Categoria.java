7ffdd4af75e86bb8b437d05338e22c7648adadc0
rnsanti <nsantiago20780@ufide.ac.cr>
2025-03-27 21:04:40 -0600

    Clase 11
    Esta vez si lo subo a tiempo jaja

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -1,10 +1,14 @@
 package com.tienda.domain;
-import jakarta.persistence.*; import java.io.Serializable; import lombok.Data;
+
+import jakarta.persistence.*; 
+import java.io.Serializable; 
+import lombok.Data;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "categoria")
-public class Categoria implements serializable {
+public class Categoria implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
@@ -13,10 +17,13 @@ public class Categoria implements serializable {
     @Column(name = "id_categoria")
     private Long idCategoria;
     
-    
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
+    
+    @OneToMany
+    @JoinColumn(name = "id_categoria", updatable = false)
+    List<Producto> productos;
     
     public Categoria() {
     }
