d31ce9add23f695bce467f2ac59da6f593daeb41
rnsanti <nsantiago20780@ufide.ac.cr>
2025-04-11 11:42:28 -0600

    Clase 13
    Commit clase 13

---
+++ src/main/java/com/tienda/dao/VentaDao.java
@@ -0,0 +1,7 @@
+package com.tienda.dao;
+ 
+import com.tienda.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface VentaDao extends JpaRepository <Venta,Long> {
+}
